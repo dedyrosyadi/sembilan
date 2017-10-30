2754f336bf85248f7a9eccc7ca092f9d5095bc41
dedyrosyadi <dedy.rosyadi@gmail.com>
2017-10-30 20:23:11 +0800

    perbaiki beberapa css

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
-console.log("gulp bootstrap starter");
+// console.log("gulp bootstrap starter");
+$('.page-scroll').on('click', function(e) {
+	var target = $(this).attr('href');
+
+	// $('body').scrollTop(target.offset().top);
+	$('html, body').animate({
+		scrollTop: $(target).offset().top - 65
+	}, 800);
+
+	e.preventDefault();
+
+});
+
+
+function openNav() {
+	$("#menu-resv").animate({height: "100%"});
+}
+
+function closeNav() {
+	$("#menu-resv").animate({height: "0"});
+}
+
+
+$(document).on("scroll", function() {
+
+	if($(document).scrollTop()>50) {
+		$("header").removeClass("large").addClass("small");
+	} else {
+		$("header").removeClass("small").addClass("large");
+	}
+
+});
